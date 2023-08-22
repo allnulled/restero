6716354c5e6dbe3287d83ed370b818f1f16a46cc
allnulled <todosanulados@gmail.com>
2023-08-22 13:51:42 +0200

    base de datos conectable

--- src/utilities/controlador_de_operacion.js
+++ src/utilities/controlador_de_operacion.js
@@ -2,19 +2,31 @@ module.exports = function (deployer) {
     return async function (request, response, next) {
         try {
             const { operation, table } = request.params;
-            ///////////////////////////////////////////////////////
-            ///////////////////////////////////////////////////////
-            ///////////////////////////////////////////////////////
-            ///////////////////////////////////////////////////////
-            ///////////////////////////////////////////////////////
-            ///////////////////////////////////////////////////////
-            //////////////////////GOGOGO///////////////////////////
-            ///////////////////////////////////////////////////////
-            ///////////////////////////////////////////////////////
-            ///////////////////////////////////////////////////////
-            ///////////////////////////////////////////////////////
-            ///////////////////////////////////////////////////////
-            ///////////////////////////////////////////////////////
+            if(operation === "select") {
+                const conexion = await deployer.utilities.obtener_conexion_de_base_de_datos();
+                console.log(conexion);
+                const resultado = await conexion.ejecutar("SELECT 100;");
+                request.hql_data.resultado = resultado;
+                // @TODO: montar query
+                // @TODO: recoger conexión a base de datos
+                // @TODO: lanzar query
+                // @TODO: devolver datos de query
+            } else if (operation === "insert") {
+                // @TODO: montar query
+                // @TODO: recoger conexión a base de datos
+                // @TODO: lanzar query
+                // @TODO: devolver datos de query
+            } else if (operation === "update") {
+                // @TODO: montar query
+                // @TODO: recoger conexión a base de datos
+                // @TODO: lanzar query
+                // @TODO: devolver datos de query
+            } else if (operation === "delete") {
+                // @TODO: montar query
+                // @TODO: recoger conexión a base de datos
+                // @TODO: lanzar query
+                // @TODO: devolver datos de query
+            }
             response.json({
                 operation,
                 table,
@@ -23,7 +35,7 @@ module.exports = function (deployer) {
         } catch (error) {
             console.error("Error en «src/utilities/controlador_de_operacion.js»");
             console.error(error);
-            deployer.utilities.gestor_de_error_de_peticion(request, error);
+            deployer.utilities.gestor_de_error_de_peticion(response, error);
         }
     };
 };
---
+++ src/utilities/obtener_conexion_de_base_de_datos.js
@@ -0,0 +1,48 @@
+const sqlite3 = require("sqlite3");
+
+module.exports = function (deployer) {
+    return async function () {
+        try {
+            if (deployer.db.conexion) {
+                return deployer.db.conexion;
+            }
+            let conexion_bruta = undefined;
+            await new Promise(function (ok, fail) {
+                conexion_bruta = new sqlite3.Database(__dirname + "/../configurations/db.instancia.sqlite", (error) => {
+                    if (error) {
+                        return fail(error);
+                    }
+                    return ok();
+                });
+            });
+            const conexion_neta = {
+                ejecutar: function (consulta_unica) {
+                    return new Promise(function (ok, fail) {
+                        try {
+                            conexion_bruta.serialize(function () {
+                                try {
+                                    conexion_bruta.all(consulta_unica, [], function (error, data) {
+                                        if (error) {
+                                            return fail(error);
+                                        }
+                                        return ok(data);
+                                    });
+                                } catch (error) {
+                                    return fail(error);
+                                }
+                            });
+                        } catch (error) {
+                            return fail(error);
+                        }
+                    });
+                }
+            }
+            deployer.db.conexion = conexion_neta;
+            return conexion_neta;
+        } catch (error) {
+            console.error("Error en «src/utilities/obtener_conexion_de_base_de_datos.js»");
+            console.error(error);
+            throw error;
+        }
+    };
+};
